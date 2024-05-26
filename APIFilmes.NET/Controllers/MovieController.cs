d58eafe4a202519968ea1795efb378ba8f628ffe
Samuel Matos <s.rodriguesmt03@gmail.com>
2024-05-26 15:12:29 -0300

    Criando uma API de estudos

---
+++ APIFilmes.NET/Controllers/MovieController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiFilmes.Models;
+
+namespace WebApiFilmes.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+//[Route("api/movie")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> _movies = new List<Movie>();
+    private static int id = 0;
+
+    [HttpGet]
+    public IEnumerable<Movie> GetAllMovies()
+    {
+        return _movies;
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetMovieById(int id)
+    {
+        var movie = _movies.FirstOrDefault(movie => movie.Id == id);
+
+        if (movie == null) return NotFound();
+        return Ok(movie);
+    }
+
+    [HttpGet("page")]
+    public IActionResult GetMoviesByPage([FromQuery] int _skip = 0, [FromQuery] int _take = 50)
+    {
+        // Skip -> Quantos elementos da lista voce quer pular
+        // Take -> Quantos elementos da lista vcoce quer pegar
+        //return _movies.Skip(_skip).Take(_take);
+
+        var movies = _movies.Skip(_skip).Take(_take);
+        if (movies == null) return NotFound();
+        return Ok(movies);
+    }
+
+    [HttpPost]
+    public IActionResult CreateMovie([FromBody] Movie movie)
+    {
+        movie.Id = id++;
+
+        _movies.Add(movie);
+
+        return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, movie);
+
+        //Console.WriteLine("Titulo: " + movie.Title);
+        //Console.WriteLine("Gênero: " + movie.Gender);
+        //Console.WriteLine("Tempo de Duração: " + movie.DurationTime);
+    }
+}
---
+++ APIFilmes.NET/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiFilmes.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O Título do Filme é Obrigatório")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "O Gênero do Filme é Obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do Gênero não pode exceder 50 caracteres")]
+    public string Gender { get; set; }
+
+    [Required]
+    [Range(70, 600, ErrorMessage = "A Duração deve ter entre 70 e 600 minutos")]
+    public int DurationTime { get; set; }
+}
